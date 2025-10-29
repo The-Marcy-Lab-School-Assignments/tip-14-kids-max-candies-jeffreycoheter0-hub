9cf693b1dcd8b835a7c1d5f6a70069653bf245a3
Jeffrey Cohetero <jeffreycoheter0@gmail.com>
2025-10-29 14:18:59 -0400

    Added comments to index.js file

--- index.js
+++ index.js
@@ -11,6 +11,22 @@
  */
 function kidsWithCandies(candies, extraCandies) {
 
+    /* 
+    We create a new var 'greatest' that is able to be changed at anytime.
+    We then use the 'Math.max()' method to find the highest
+    number in the array ('s - the spead operator '...' allows us to
+    check an infinite ammount of arrays)
+    */
+    let greatest = Math.max(...candies);
+
+    /* 
+    We create a new array but want to keep the same length, so '.map()' is 
+    the perfect method.
+    We look through the elements in the origianl 'candies' array, and check if they are
+    greater than or equal too the 'greatest' number in the array.
+    We then change the elements in the new array with booleans, 'true' or 'false'
+    */
+    return candies.map((el) => el + extraCandies >= greatest);
 }
 
 module.exports = kidsWithCandies;
